e24d00a483b7ae48d6d52afbb0bffd1cade943c4
Saarmets <ralf.saarmets@gmail.com>
2021-12-12 18:34:30 +0200

    Update FattyBird.py
    uus resolutsioon

--- FattyBird.py
+++ FattyBird.py
@@ -2,12 +2,8 @@ import pygame, pygame_gui, sys, random
 
 pygame.init()
 
-aken = pygame.display.set_mode([750, 1334])
-manager = pygame_gui.UIManager([750, 1334])
-
-sulgenupp = pygame_gui.elements.UIButton(pygame.Rect((705, 125), (50, 50)),
-                                         'Sulge',
-                                         manager)
+aken = pygame.display.set_mode([576, 1024])
+manager = pygame_gui.UIManager([576, 1024])
 
 kell = pygame.time.Clock()
 töötab = True
@@ -15,11 +11,8 @@ töötab = True
 while töötab:
     dt = kell.tick() / 1000
     for e in pygame.event.get():
-        if e.type == pygame.USEREVENT:
-            if e.user_type == pygame_gui.UI_BUTTON_PRESSED: 
-                if e.ui_element == sulgenupp:
-                    print("test")
-                    töötab = False
+        if e.type == pygame.QUIT:
+            töötab = False
             
         manager.process_events(e)
     
