b2ea0a374c671b7a8ec3f4aa671efc5b8683d6b5
Saarmets <ralf.saarmets@gmail.com>
2021-12-12 18:23:49 +0200

    Update FattyBird.py
    Sulgemis värk

--- FattyBird.py
+++ FattyBird.py
@@ -1,2 +1,31 @@
-print("Uus test")
-print("test2")
+import pygame, pygame_gui, sys, random
+
+pygame.init()
+
+aken = pygame.display.set_mode([750, 1334])
+manager = pygame_gui.UIManager([750, 1334])
+
+sulgenupp = pygame_gui.elements.UIButton(pygame.Rect((705, 125), (50, 50)),
+                                         'X',
+                                         manager)
+
+kell = pygame.time.Clock()
+töötab = True
+
+while töötab:
+    dt = kell.tick() / 1000
+    for e in pygame.event.get():
+        if e.type == pygame.USEREVENT:
+            if e.user_type == pygame_gui.UI_BUTTON_PRESSED: 
+                if e.ui_element == sulgenupp:
+                    print("test")
+                    töötab = False
+            
+        manager.process_events(e)
+    
+    aken.fill([255, 255, 255])
+    manager.update(dt) 
+    manager.draw_ui(aken)
+    pygame.display.flip()
+pygame.quit()
+sys.exit()
