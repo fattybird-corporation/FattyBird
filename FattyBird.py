8e3484c77815f000dbf943233d5edaa59c6d0656
Saarmets <ralf.saarmets@gmail.com>
2021-12-18 15:35:38 +0200

    Update FattyBird.py
    Tausta valik, paar kommentaari

--- FattyBird.py
+++ FattyBird.py
@@ -1,6 +1,6 @@
-import pygame, pygame_gui, sys, random #impordin pygame, pygame gui, sys ja randomi
+import pygame, pygame_gui, sys, random # Impordib vajalikud moodulid
 
-pygame.init()#käivitab pygame'i
+pygame.init()
 
 aken = pygame.display.set_mode([576, 1024]) # Teeb akna ja laeb pildid
 manager = pygame_gui.UIManager([576, 1024])
@@ -15,8 +15,16 @@ alumine_post = pygame.image.load("post1.png")
 power_tiib = pygame.image.load("power_up1.png")
 power_kilp = pygame.image.load("power_up2.png")
 
+taustanumber = random.randint(1,3) # Valib kolmest valikust suvalise tausta mida näidata
+if taustanumber == 1:
+    taust = taust1
+elif taustanumber == 2:
+    taust = taust2
+elif taustanumber == 3:
+    taust = taust3
+
 kell = pygame.time.Clock()
-töötab = True# käivitab mängu
+töötab = True
 
 while töötab:
     dt = kell.tick() / 1000
@@ -27,6 +35,7 @@ while töötab:
         manager.process_events(e)
     
     aken.fill([255, 255, 255])
+    aken.blit(taust,(0,0))
     manager.update(dt) 
     manager.draw_ui(aken)
     pygame.display.flip()
