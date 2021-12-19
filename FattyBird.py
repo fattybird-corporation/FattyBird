9f6bba76c33aba0dd61b6c065422967449b6fe77
KaarelPeppa <kaarel.lepa@gmail.com>
2021-12-19 12:52:52 +0200

    Update FattyBird.py
    Pooleli olev powerup

--- FattyBird.py
+++ FattyBird.py
@@ -1,3 +1,7 @@
+import os
+kaust = os.path.dirname(os.path.realpath(__file__))
+os.chdir(kaust)
+
 import pygame, sys, random # Impordib vajalikud moodulid
 
 pygame.init()
@@ -42,6 +46,8 @@ kõrgeim_skoor = 0
 gravitatsioon = 0.5
 linnu_liikumine = 0
 lisaskoor = True
+tüüp = 0
+powerLiigu = True
 
 taustanumber = random.randint(1,3) # Valib kolmest valikust suvalise tausta mida näidata
 if taustanumber == 1:
@@ -76,7 +82,7 @@ def joonista_postid(postid): # Funktsioon mis joonistab postid
             aken.blit(tagurpidi_post,post)
             
 def joonista_power_up():
-    global power_kilp,power_tiib 
+    global power_kilp,power_tiib, tüüp, powerup_rect
     
     tüüp = random.randint(1,2)
     powerup_y = random.randint(400,800)
@@ -84,10 +90,12 @@ def joonista_power_up():
     powerup_rect = pygame.Rect(powerup_x, powerup_y, 88, 86)
     powerupid = []
     powerupid.append(powerup_rect)
-    if tüüp == 1:# kui power up on esimene
-            aken.blit(power_tiib,powerup_rect)
+    '''if tüüp == 1:# kui power up on esimene
+        pass
+        #aken.blit(power_tiib,powerup_rect)
     elif tüüp == 2:# kui power up on teine
-            aken.blit(power_kilp,powerup_rect)
+        pass
+        #aken.blit(power_kilp,powerup_rect)'''
 
 def kuva_skoor(mängu_olek):
     if mängu_olek == "elus":
@@ -158,8 +166,10 @@ while töötab: # Mängu tsükkel
             töötab = False
         if e.type == UUSPOST:
             posti_list.extend(ehita_post())
-        if skoor == 4:
-            joonista_power_up()
+        if skoor == 2:
+            if powerLiigu:
+                joonista_power_up()
+                powerLiigu = False
         if e.type == pygame.KEYDOWN:
             if e.key == pygame.K_SPACE and elus: # Kui vajutatakse tühikut
                 linnu_liikumine = 0
@@ -182,6 +192,12 @@ while töötab: # Mängu tsükkel
     
     aken.fill([255, 255, 255])
     aken.blit(taust,(0,0))
+    
+    if elus:
+        if tüüp == 1:
+            aken.blit(power_kilp, powerup_rect)
+        elif tüüp == 2:
+            aken.blit(power_tiib, powerup_rect)
     
     if elus:
         # Lind
